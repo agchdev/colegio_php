77ace4a017bc151a47221970d21f4e0af5064a1c
unknown <alex.chinchi02@gmail.com>
2025-01-20 11:13:12 +0100

    cambios

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php
+    function mostrarCalificar() {
+        require_once('class.alumno.php');
+        require_once('class.asignatura.php');
+        $asignatura = new asignatura();
+        $asignaturas = $asignatura->obtenerAsignaturas();
+        $alumno = new alumno();
+        $alumnos = $alumno->obtenerAlumnos();
+        if(isset($_POST["alumno"])) {
+            mostrarCalificacionesAlumnos($_POST["alumno"], $_POST["nomAlum"], $_POST["dniAlum"]);
+        }else{
+            require_once('calificar.php');
+        }
+    }
+    function mostrarCalificacionesAlumnos($idAlumno, $aluNom = "", $aluDni = "") {
+        require_once('class.alumno.php');
+        require_once('class.asignatura.php');
+        $asignatura = new asignatura();
+        $asignaturas = $asignatura->obtenerAsignaturas();
+        $alumno = new alumno();
+        $alumnos = $alumno->obtenerAlumnos();
+        require_once('class.aluasig.php');
+        $aluasig = new aluasig();
+        $aluCalificaciones = $aluasig->obtenerDatos($idAlumno);
+        require_once('calificar.php');
+    }
     function opcion() {
-        require_once("modelo.php");
         if(isset($_POST["nota"])) {
             mostrarCalificar();
         }else if(isset($_POST["exp"])) {
@@ -8,7 +32,6 @@
         }
     }
     function calificar() {
-        require_once("modelo.php");
         mostrarCalificar();
     }
     if(isset($_REQUEST["action"])) {
